bd10a2b2e4137e87a443228440b0266c5f170727
ant-chantrell <ant.chantrell@outlook.com>
2022-10-30 20:39:07 +0000

    Hello

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your Name: ");
+
+            string Name = Console.ReadLine();
+
+            Console.WriteLine("Hello " + Name + " I hope you are well.");
+
+        }
+    }
+}
